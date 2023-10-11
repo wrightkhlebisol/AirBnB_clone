434b434e5cd910149e236a2185c1614e621c0558
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-11 21:59:13 +0100

    recreate class from dict

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel:
     """common attributes/methods for other classes
     """
     def __init__(self, *args, **kwargs):
-        """public instance method
+        """public instance attributes
         """
         if len(kwargs) != 0:
             for key, value in kwargs.items():
@@ -17,9 +17,12 @@ class BaseModel:
                     continue
                 if key == 'created_at' or key == 'updated_at':
                     self.__dict__[key] = datetime.fromisoformat(value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """string method
