dce1e54e614bbfab6ee429b1951d034264347d96
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-11 04:56:05 +0100

    implemented *args and **kwargs in the instance method

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,15 @@ from datetime import datetime
 class BaseModel:
     """common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """public instance method
         """
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.fromisoformat(value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -35,6 +41,8 @@ class BaseModel:
     def to_dict(self):
         """returns dictionary containing all keys/values
         of __dict__ of the instance
+
+        Returns: __dict__ (dictionary)
         """
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
