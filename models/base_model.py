eddf5a38b7b3d15c0836512c2d0db396f67b064e
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-11 04:21:42 +0100

    add save and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,31 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """string method
+
+            Retuns: classname (str)
+                    id (str)
+                    dictionary (str)
         """
         class_name = self.__class__.__name__
         obj_id = self.id
         obj_dict = self.__dict__
-        return f"[{class_name}] ({obj_id}) {obj_dict}" 
+        return f"[{class_name}] ({obj_id}) {obj_dict}"
+
+    def save(self):
+        """updates public instance variable updated_at
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
