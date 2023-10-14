07a381116f29bad6aa7fb2aa1b8450496c968afe
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-14 19:51:35 +0100

    implement user model

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""User Model that inherits from BaseModel """
+from .base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User Model """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+
