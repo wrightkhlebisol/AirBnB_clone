3256a48531a6b87926915e98809e4b51ffef79df
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-14 10:58:10 +0100

    import models

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """BaseModel that defines all common attributes/methods
 """
-from . import storage
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """string method
@@ -42,7 +42,7 @@ class BaseModel:
         """updates public instance variable updated_at
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns dictionary containing all keys/values
