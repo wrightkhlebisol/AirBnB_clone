08c263d19240602de02fcbedf254b6b39c188229
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-15 23:10:50 +0100

    import various modules

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,14 @@
 file and deserializes JSON file to instances """
 import json
 import os
-from models import base_model, user, amenity, city, place, review, state
+
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage():
