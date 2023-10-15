249cf24911abab396e03f2d9be5dd0990626842e
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-15 23:37:43 +0100

    made some changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,11 +6,6 @@ import os
 
 from models.base_model import BaseModel
 from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 
 
 class FileStorage():
