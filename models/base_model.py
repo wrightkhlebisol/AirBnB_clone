5d7803e7a24e1db54100367364b416af1a0602b1
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-26 00:19:45 +0000

    convert back to object from json

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,22 @@ from datetime import datetime
 class BaseModel:
     """Base Model for all classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize instances."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if (kwargs):
+            for key in kwargs.keys():
+                if key == '__class__':
+                    continue
+
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.fromisoformat(kwargs[key])
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Update updated_at with current datetime."""
